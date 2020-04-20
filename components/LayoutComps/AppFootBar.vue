c6be955e8c84a952382634b71804e42f05d58713
tboyd4 <tboyd1898@gmail.com>
2020-04-20 14:57:04 -0500

    added header and footer bars to the default vue

---
+++ components/LayoutComps/AppFootBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app-bar color="primary" dark flat dense>
+        <v-container>
+            <v-row>
+                <v-col><v-toolbar-title class="text-center">&copy; 2020 Design Source Flooring, LLC</v-toolbar-title></v-col>
+            </v-row>
+        </v-container>
+        
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/LayoutComps/AppNavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app-bar color="primary" dark flat dense>
+        <v-toolbar-title>DSF Invoice Mailing System</v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
